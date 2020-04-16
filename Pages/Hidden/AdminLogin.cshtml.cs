681b06e9789427ee05951b80aa0eb0e08906b482
Michał Białek <m.biaek91@gmail.com>
2020-04-17 01:46:39 +0200

    Modify main page, star creating database-code first.

---
+++ Pages/Hidden/AdminLogin.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SushiRestaurant.data;
+
+namespace Sushi_Restaurant.Pages.Hidden
+{
+    public class AdminLoginModel : PageModel
+    {
+        private readonly IUserData userData;
+        private readonly IHtmlHelper htmlHelper;
+
+        public AdminLoginModel(IUserData userData, IHtmlHelper htmlHelper)
+        {
+            this.userData = userData;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
